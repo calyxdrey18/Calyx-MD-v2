754e1fee62376c61f6187cf99738859d4dede362
Calyx <calyxdrey11@gmail.com>
2025-05-15 13:27:23 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Calyx-MD,
+  author: '‎',
+  botName: "Calyx-MD",
+  botOwner: 'Calyx-Drey, // Your name
+  ownerNumber: '263716558758', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+};
+
+module.exports = settings;
